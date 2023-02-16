9d73af33b9d738e26c8c1019e09a64b51ff69faa
Sergey Kravchenko <g605413@gmail.com>
2023-02-16 22:17:31 +0300

    feat: add saving initialized admins to json

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,5 @@
+import json
+
 from telegram.ext.filters import MessageFilter
 
 from handlers.response import get_chat_id
@@ -5,7 +7,21 @@ import logger
 
 logger = logger.get_logger(__name__)
 
-admins = list()  # TODO: Сделать сохранение админов
+
+def save_admins():
+    with open("admins.json", "w") as fp:
+        json.dump(admins, fp)
+
+
+def read_admins() -> list:
+    with open("admins.json", "r") as fp:
+        return json.load(fp)
+
+
+try:
+    admins = read_admins()
+except FileNotFoundError:
+    admins = list()
 
 
 class Filter(MessageFilter):
@@ -15,12 +31,15 @@ class Filter(MessageFilter):
 
 
 def create(user_id) -> None:
+    assert not is_admin(user_id), "User is already an admin"
     admins.append(user_id)
+    save_admins()
     logger.debug(f"Added admin: {user_id}")
 
 
 def delete(user_id) -> None:
     admins.remove(user_id)
+    save_admins()
     logger.debug(f"Deleted admin: {user_id}")
 
 
